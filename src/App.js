23e93669be2f5d67c5e6bc536d40716646e1f9e4
Abhishek <abhishek404singh@gmail.com>
2021-11-15 00:33:19 +0530

    styled-components

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import Links from "./components/Links";
+import LogoImg from "./components/Logo";
+import "./App.css";
+function App() {
+  return (
+    <div className="main">
+      <div>
+        <LogoImg>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH1qgWfKaFQucxcGCmMb3buBnO_s6DkL_vm8p4qAW_jFvmKWyb20wWmt0xwYfuHGTSdY8&usqp=CAU" />
+        </LogoImg>
+      </div>
+      <div className="App">
+        <Links>Sign Up</Links>
+        <Links>Sign In </Links>
+      </div>
+    </div>
+  );
+}
+
+export default App;
